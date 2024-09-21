46f178b1ca3d6191bd63003d617e1239ee8d026c
Jhosua Lascarro <jdlascarroa24@ul.edu.co>
2024-09-21 10:49:01 -0500

    no

--- modules/busqueda_lineal.py
+++ modules/busqueda_lineal.py
@@ -1,4 +1,4 @@
 def busqueda_lineal(lista, objeto):
     for i in range(len(lista)):
-        if lista[i] == objeto: return i
-    return 0-1
+        if lista[i] == objeto: return i # 1. if lista[i]%2 == 0
+    return -1
