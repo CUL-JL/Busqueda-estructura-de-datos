858298747b413e918286ecd29613a73ea8127bd4
Jhosua Lascarro <jdlascarroa24@ul.edu.co>
2024-09-21 10:18:40 -0500

    no

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from modules.busqueda_lineal import busqueda_lineal
 from modules.busqueda_binaria import busqueda_binaria
+
 # 1. 
+
+list_elemnts = [3,1,4,6,7,5,9,10]
+busqueda_lineal(list_elemnts, 2 )
